6a2159116d6d5465f1dc00846ce740e43052e80b
Owen <owenmccr@gmail.com>
2023-09-03 15:18:28 -0400

    mostly complete, need to add character limit

---
+++ script.js
@@ -0,0 +1,47 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword(length, characters) {
+  var password = "";
+  for (i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var symbols = "!@#$%^&*_-+=";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var characters = "";
+  if (confirm("Would you like your password to include lowercase letters?")) {
+    characters += lowercase;
+  }
+  if (confirm("Would you like your password to include uppercase letters?")) {
+    characters += uppercase;
+  }
+  if (confirm("Would you like to add numbers to your password?")) {
+    characters += numbers;
+  }
+  if (confirm("Would you like to add symbols to your password?")) {
+    characters += symbols;
+  }
+  if (characters !== "") {
+    var length = prompt("How long would you like your password to be?");
+  } else {
+    alert("You must select at least one character type!");
+  }
+
+  var password = generatePassword(length, characters);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
